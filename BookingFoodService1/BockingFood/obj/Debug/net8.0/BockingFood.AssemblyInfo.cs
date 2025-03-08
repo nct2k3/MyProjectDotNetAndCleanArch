e5d472b9de8a4e1ac6b0b754b887598b9caefcc7
T14 Gen 3 <nguyennrdz@gmail.com>
2025-03-08 15:56:30 +0700

    8/3

--- BookingFoodService1/BockingFood/obj/Debug/net8.0/BockingFood.AssemblyInfo.cs
+++ BookingFoodService1/BockingFood/obj/Debug/net8.0/BockingFood.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BockingFood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6bfbcf031e19ae796d9c87c39f692d84b43cae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6ba621ca10191b482d00a6937fcf82e2ee00e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BockingFood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BockingFood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
